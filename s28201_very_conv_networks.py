19399674da330431bf24de90789f746f1d55a734
volodymyr <s28201@pjwstk.edu.pl>
2025-05-18 15:13:44 +0200

    Minor fix

--- s28201_very_conv_networks.py
+++ s28201_very_conv_networks.py
@@ -16,7 +16,7 @@ ASL_CLASS_NAMES = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', '
                    'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
                    'nothing', 'space']
 IMG_SIZE = (16, 16)
-TRAIN_NEW_MODEL = False
+TRAIN_NEW_MODEL = True
 
 print("Path to ASL dataset files:", ASL_PATH)
 
